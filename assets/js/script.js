6ac6233e6c0348b2cfcbbbc70bae1ac224bb1577
Tomek Regulski <tomek.regulski@gmail.com>
2021-02-27 21:38:01 -0500

    basic function for time comparison/styling to show past/present/future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,46 @@
 var dayDis = $("#currentDay");
 var textArea = $("textarea");
+var time3 = $('#time11');
+var time4 = $('#time12');
+var now = moment();
 
 function handleTimeDisplay() {
     var day = moment().format("dddd, MMMM Do");
     dayDis.text(day);
 };
+// };
+// console.log(time3.text());
+// handleTimeDisplay();
+// textAreaColor();
 
-handleTimeDisplay();
-textAreaColor();
-
-function textAreaColor() {
-    textArea.setAttribute("style", "background-color: green");
+// function textAreaColor() {
+//     textArea.setAttribute("style", "background-color: green");
+// };
+function past() {
+    var beginningTime = moment(time3.text(), 'h:mma');
+    var endTime = moment();
+    if (beginningTime.isBefore(endTime)) {
+        // console.log((beginningTime.isBefore(endTime)));
+        $( "#task11" ).css("background-color", "yellow");
+    }
 };
+
+function current() {
+    var beginningTime = moment(time3.text(), 'h:mma');
+    var endTime = moment(time3.text(), 'h:mma');
+    if (beginningTime.isSame(endTime)) {
+        // console.log(beginningTime.isSame(endTime));
+    }
+};
+
+function future() {
+    var beginningTime = moment(time4.text(), 'h:mma');
+    var endTime = moment();
+    if (beginningTime.isAfter(endTime)) {
+        console.log((beginningTime.isAfter(endTime)));
+        $( "#task12" ).css("background-color", "green");
+    }
+};
+past();
+current();
+future;
